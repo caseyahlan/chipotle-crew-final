afb32b6d3e27f57d452da7ae1b530f3625076b7a
Casey Lum <caseylum@uw.edu>
2017-03-03 17:41:03 -0800

    start

--- server.R
+++ server.R
@@ -3,6 +3,101 @@ library("ggplot2")
 library("shiny")
 library("plotly")
 
+base <- ("https://congress.api.sunlightfoundation.com/")
+
 server <- function(input, output) {
+  legislators <- reactive({
+    resource <- ("legislators/locate")
+    query <- paste0("?zip=", input$zip)
+    response <- GET(paste0(base, resource, query))
+    body <- fromJSON(content(response, "text"))
+    legislators <- flatten(body$results) %>% mutate(name = paste(first_name, last_name)) %>% select(name, chamber, party, state, phone, website)
+    return(legislators)
+  })
+  
+  output$reps <- renderTable({
+    return(legislators())
+  })
+  
+  output$zipcode <- reactive({
+    return(paste0(input$zip, ":"))
+  })
+  
+  output$photos <- renderUI({
+    resource <- ("legislators/locate")
+    query <- paste0("?zip=", input$zip)
+    response <- GET(paste0(base, resource, query))
+    body <- fromJSON(content(response, "text"))
+    bio.ids <- flatten(body$results) %>% select(bioguide_id)
+    picture.base <- ("https://theunitedstates.io/images/congress/225x275/")
+    picture.query <- (".jpg")
+    num.reps <- nrow(bio.ids)
+    if (num.reps ==1 ) {
+      picture1 <-paste0(picture.base, bio.ids[1,1], picture.query)
+      tags$img(src=picture1)
+    } else if (num.reps == 2) {
+      picture1 <-paste0(picture.base, bio.ids[1,1], picture.query)
+      picture2 <-paste0(picture.base, bio.ids[2,1], picture.query)
+      tagList(tags$img(src=picture1), 
+              tags$img(src=picture2))
+    } else if (num.reps == 3) {
+      picture1 <-paste0(picture.base, bio.ids[1,1], picture.query)
+      picture2 <-paste0(picture.base, bio.ids[2,1], picture.query)
+      picture3 <- paste0(picture.base, bio.ids[3,1], picture.query)
+      tagList(tags$img(src=picture1), 
+              tags$img(src=picture2), 
+              tags$img(src=picture3)) }
+    else if (num.reps == 4) {
+      picture1 <-paste0(picture.base, bio.ids[1,1], picture.query)
+      picture2 <-paste0(picture.base, bio.ids[2,1], picture.query)
+      picture3 <- paste0(picture.base, bio.ids[3,1], picture.query)
+      picture4 <- paste0(picture.base, bio.ids[4,1], picture.query)
+      tagList(tags$img(src=picture1, width = 187.5),
+              tags$img(src=picture2, width = 187.5),
+              tags$img(src=picture3, width = 187.5), 
+              tags$img(src=picture4, width = 187.5)) 
+    } else if (num.reps == 5) {
+      picture1 <-paste0(picture.base, bio.ids[1,1], picture.query)
+      picture2 <-paste0(picture.base, bio.ids[2,1], picture.query)
+      picture3 <- paste0(picture.base, bio.ids[3,1], picture.query)
+      picture4 <- paste0(picture.base, bio.ids[4,1], picture.query)
+      picture5 <- paste0(picture.base, bio.ids[5,1], picture.query)
+      tagList(tags$img(src=picture1, width=150), 
+              tags$img(src=picture2, width=150), 
+              tags$img(src=picture3, width=150), 
+              tags$img(src=picture4, width=150), 
+              tags$img(src=picture5, width=150)) 
+    } else if (num.reps == 6) {
+      picture1 <-paste0(picture.base, bio.ids[1,1], picture.query)
+      picture2 <-paste0(picture.base, bio.ids[2,1], picture.query)
+      picture3 <- paste0(picture.base, bio.ids[3,1], picture.query)
+      picture4 <- paste0(picture.base, bio.ids[4,1], picture.query)
+      picture5 <- paste0(picture.base, bio.ids[5,1], picture.query)
+      picture6 <- paste0(picture.base, bio.ids[6,1], picture.query)
+      tagList(tags$img(src=picture1, width=125), 
+              tags$img(src=picture2, width=125),
+              tags$img(src=picture3, width=125), 
+              tags$img(src=picture4, width=125), 
+              tags$img(src=picture5, width=125), 
+              tags$img(src=picture6, width=125))
+    } else if (num.reps == 7) {
+      picture1 <-paste0(picture.base, bio.ids[1,1], picture.query)
+      picture2 <-paste0(picture.base, bio.ids[2,1], picture.query)
+      picture3 <- paste0(picture.base, bio.ids[3,1], picture.query)
+      picture4 <- paste0(picture.base, bio.ids[4,1], picture.query)
+      picture5 <- paste0(picture.base, bio.ids[5,1], picture.query)
+      picture6 <- paste0(picture.base, bio.ids[6,1], picture.query)
+      picture7 <- paste0(picture.base, bio.ids[7,1], picture.query)
+      tagList(tags$img(src=picture1, width=107),
+              tags$img(src=picture2, width=107),
+              tags$img(src=picture3, width=107),
+              tags$img(src=picture4, width=107),
+              tags$img(src=picture5, width=107), 
+              tags$img(src=picture6, width=107), 
+              tags$img(src=picture7, width=107))
+    }
+    
+  })
+  
   
 }
--- ui.R
+++ ui.R
@@ -6,5 +6,49 @@ library("dplyr")
 ui <- fluidPage(
   titlePanel(em("Title")),
   h3("By Kelsey Kua, Casey Lum, and Devin Reich"),
-  h5("This report is about blah blah blah")
-)
+  h5("This report is about blah blah blah"),
+  img(src="flag.jpg", height=245), 
+  img(src="capitolbuilding.jpg", height=245), 
+  img(src="congress.jpg", height=245), hr(),
+  
+  sidebarLayout(
+    
+    sidebarPanel(
+      h3("Parameters"),
+      textInput('zip', "Zipcode", value="90210"),
+      h5(em("Enter a zipcode to view legislators from the district(s) within that zipcode.")) 
+      ),
+    mainPanel(
+      tabsetPanel(type="tabs",
+                  tabPanel("Your Representatives",
+                           h3("Your Representatives"),
+                           ("Below are the members of Congress that represent the zipcode"),
+                           textOutput('zipcode', inline=TRUE),
+                           tableOutput('reps'),
+                           uiOutput('photos')),
+                  
+                  tabPanel("Compare Representatives"),
+                  
+                  tabPanel("Voting Record"),
+                  
+                  tabPanel("View a Vote"),
+                  
+                  tabPanel("Vote Breakdown"),
+                  
+                  tabPanel("Gender Makeup"),
+                  
+                  tabPanel("Party Makeup"),
+                  
+                  tabPanel("Voting Reliability")
+                  ))
+      ),
+  hr(),
+  ("Image credits for header photos (L to R):"), 
+  tags$a(href="http://feelgrafix.com/group/american-flag.html", "feelgrafix", target = "_blank"), 
+  ("|"),
+  tags$a(href="https://en.wikipedia.org/wiki/United_States_Congress", "Wikipedia", target = "_blank"), 
+  ("|"),
+  tags$a(href="https://www.brookings.edu/multi-chapter-report/vital-statistics-on-congress/", "Brookings", target = "_blank"), br()
+  
+  
+  )
