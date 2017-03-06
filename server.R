b6a6dc140315cb489354f8fb4d66d3851301761e
Devin Reich <devin.d.reich@gmail.com>
2017-03-05 17:18:07 -0800

    Add Testing

--- server.R
+++ server.R
@@ -11,6 +11,7 @@ library(mapdata)
 library(sp)
 library(geojsonio)
 library(curlconverter)
+library(tidyr)
 
 state <- geojson_read("stateData.geojson", what = "sp")
 
@@ -131,6 +132,14 @@ x26 <- GetPartyMakeup("s80-2017") %>% select(n)
 colnames(x26)[1] <- "26"
 x <- cbind(x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15, x16, x17, x18, x19, x20,
            x21, x22, x23, x24, x25, x26)
+voter.party <- c("D", "I","R")
+x <- select(x, -voter.party)
+x <- gather(x, key = Year,
+            value  = Members,
+            1:26, convert = TRUE)
+x <- data.frame(voter.party,x)
+#ggplot(x, aes(x=Year, y=Members, fill=voter.party)) + 
+#  geom_area()
 
 server <- function(input, output) {
   output$choice <- renderUI({
